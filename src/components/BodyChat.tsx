66889c2d45e382c2d05c4291affc2841a638ddf5
vladyslav-yavorskyi <vlad09vlad09@gmail.com>
2023-11-24 09:10:47 +0100

    messages in body chat

--- src/components/BodyChat.tsx
+++ src/components/BodyChat.tsx
@@ -1,9 +1,36 @@
 import styled from "styled-components";
-
+import {useEffect, useState} from "react";
+import Message from "./Message.tsx";
+import Conversation from "../data/conversation.json"
+export interface IMessage {
+    sender: string;
+    message: string;
+    time: string;
+}
 const BodyChat = () => {
+
+    const [messages, setMessages] = useState<IMessage[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // const response = await fetch('../data/conversation.json');
+                // console.log(response.json())
+                //
+                // const data = await response.json();
+                // console.log(data)
+                // setMessages(data)
+                setMessages(Conversation as IMessage[])
+            } catch (e) {
+                console.log(e);
+            }
+        }
+        fetchData();
+    }, []);
+
     return (
         <BodyWrapper>
-            <h1>Body Chat</h1>
+            {messages.map((message: IMessage, index: number) => <Message data={message} key={index} />)}
         </BodyWrapper>
     );
 };
@@ -11,6 +38,9 @@ const BodyChat = () => {
 const BodyWrapper = styled.div`
   flex: 1;
   padding: 20px;
+  display: flex;
+  flex-direction: column;
+  overflow-y: scroll;
 `;
 
 export default BodyChat;
